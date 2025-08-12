ac61464328033d085051dd0b19d2a0e5da90592b
Phạm Tuấn Duy <phtuanduyqn.cv@gmail.com>
2025-08-12 18:45:00 +0700

    BTVN1.java

--- BTVN/BTVN1.java
+++ BTVN/BTVN1.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package BTVN;
 
-/**
- *
- * @author Admin
- */
 import java.util.*;
 import java.util.ArrayList;
 import java.util.Collections;
